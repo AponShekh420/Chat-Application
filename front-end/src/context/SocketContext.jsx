df817ecff17baec4f6fb7ba71e4396ea2e09b928
Apon Shekh <aponshekh420@gmail.com>
2023-07-31 01:33:25 +0600

    i have change socket.io url to deploy

--- front-end/src/context/SocketContext.jsx
+++ front-end/src/context/SocketContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 import { io } from "socket.io-client";
 
-let socket = io("http://localhost:3000");
+let socket = io(process.env.REACT_APP_SITE_URL);
 
 const SocketPro = createContext();
 
